9757196e6384640f27adea0900e30a1205e68be0
sonnykaiyuenlee <81832272+sonnykaiyuenlee@users.noreply.github.com>
2022-10-09 17:24:52 -0500

    deleted important information for security

--- backend/backend/settings/base.py
+++ backend/backend/settings/base.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-tova5jx!19k+=fusf7j*!k9sqo-k%mn6&vufz)m3bsvb%%=##+'
+#SECRET_KEY = #removed for security
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
